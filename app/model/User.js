1f9f37066f86422d1b47414cba9f2b27476089c3
franklynobleC <franklyessien01@gmail.com>
2023-06-13 18:48:41 -0400

    add user model

--- app/model/User.js
+++ app/model/User.js
@@ -1,13 +1,38 @@
+const { timeStamp } = require('console')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-
-
 
 // Define  user Schema
 
 const UserSchema = new Schema({
-    firstName: {
-        type: String,
-required: true
-},
+  firstName: {
+    type: String,
+    required: true
+  },
+
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  Password: {
+    type: String,
+    required: String
+  },
+  reservations: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Reservation'
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
 })
+
+module.exports = mongoose.model('User', UserSchema)
